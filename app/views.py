db53de4901182980c025e2c33ab5ae3b461e68c6
sahithi5c7 <sahithipadmakar@gmail.com>
2024-02-13 20:36:47 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from app.forms import *
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+def registration(request):
+    ufo=UserForm()
+    pfo=ProfileForm()
+    d={'ufo':ufo,'pfo':pfo}
+
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            send_mail('Registration',
+                      'Thank u for registration',
+                      'sahithipamakar@gmail.com',
+                      [MUFDO.email],fail_silently=True)
+            
+            return HttpResponse('Registration successfull')
+        else:
+            return HttpResponse('Invalid data')
+    return render(request,'registration.html',d)
+
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            HttpResponse('Invalid credentials')
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+@login_required
+def profile_display(request):
+    un=request.session.get('username')
+    UO=User.objects.get(username=un)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':PO}
+    return render(request,'profile_display.html',d)
+
+@login_required
+def changepassword(request):
+    if request.method=='POST':
+        pw=request.POST['pw']
+        username=request.session.get('username')
+        UO=User.objects.get(username=username)
+        UO.set_password(pw)
+        UO.save()
+        return HttpResponse('Changed Password Successfully')
+    return render(request,'changepassword.html')
+
+
+def forgotpassword(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+
+        LUO=User.objects.filter(username=username)
+        if LUO:
+            UO=LUO[0]
+            UO.set_password(password)
+            UO.save()
+            return HttpResponse('Reset is Done')
+        else:
+            return HttpResponse('YOUR USERNAME IS NOT IN OUR DATABASE')
+
+
+    return render(request,'forgotpassword.html')
